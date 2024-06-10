6603fa9a30c2cbc39ff6a15aedf42fb877dcb96f
Daniel da Silva Miranda <Daniel.Miranda@netconamericas.com>
2024-06-10 09:28:00 -0300

    Erro html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, session, flash
+from flask import Flask, render_template, redirect, request, url_for, session, flash
 from datetime import datetime
 
 app = Flask(__name__)
